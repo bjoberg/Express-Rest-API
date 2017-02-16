483f81756e73944f4a87a4911da24e2e49753190
bjoberg <brett@obergmail.com>
2017-02-15 20:06:23 -0600

    Added Morgan for request logging. Added POST for /questions/:qID/answers, /questions/qID/answers/aID/vote-:dir. Added PUT for /questions/:qID/answers/aID. Added DELETE for /questions/:qID/answers/aID

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var app = express();
 var routes = require("./routes");
 
 var jsonParser = require("body-parser").json; 
+var logger = require("morgan");
+
+/**
+ * This will configure the middleware to give use status codes for our API reponses
+ * 
+ * https://www.npmjs.com/package/morgan
+ */
+app.use(logger("dev"));
 
 /**
  * When any request is received parse it as json
@@ -17,8 +25,9 @@ app.use(jsonParser());
  * When any request is received look through the routes
  * 
  * Note: this runs every request
+ * Tip: You can mount your routes to have a specific base route: `app.use("/[base-route]", routes)`. Just don't forget to configure your routes in routes.js to reflect this base route.
  */
-app.use(routes);
+app.use(routes)
 
 var port = process.env.PORT || 3000;
 
--- routes.js
+++ routes.js
@@ -31,9 +31,64 @@ router.post("/questions/", function(req, res){
  * 
  * Route for getting a specific question
  */
-router.get("/questions/:id", function(req, res){
+router.get("/questions/:qID", function(req, res){
     res.json({
-        response: "You sent me a GET request for ID " + req.params.id
+        response: "You sent me a GET request for qID " + req.params.qID
+    });
+});
+
+/**
+ * POST /questions/:id/answers
+ * 
+ * Route for creating an answer
+ */
+router.post("/questions/:qID/answers", function(req, res){
+    res.json({
+        response: "You sent me a POST request to /answers",
+        questionID: req.params.qID,
+        body: req.body
+    });
+});
+
+/**
+ * PUT /questions/:qID/answers/:aID
+ * 
+ * Edit a specific answer
+ */
+router.put("/questions/:qID/answers/:aID", function(req, res){
+    res.json({
+        response: "You sent me a PUT request to /answers",
+        questionID: req.params.qID,
+        answerID: req.params.aID,
+        body: req.body
+    });
+});
+
+/**
+ * DELETE /questions/:qID/answers/:aID
+ * 
+ * Delete a specific answer
+ */
+router.delete("/questions/:qID/answers/:aID", function(req, res){
+    res.json({
+        response: "You sent me a DELETE request to /answers",
+        questionID: req.params.qID,
+        answerID: req.params.aID
+    });
+});
+
+/**
+ * POST /questions/:qID/answers/:aID/vote-up
+ * POST /questions/:qID/answers/:aID/vote-down
+ * 
+ * Vote on a specific answer
+ */
+router.post("/questions/:qID/answers/:aID/vote-:dir", function(req, res){
+    res.json({
+        response: "You sent me a POST request to /vote-" + req.params.dir,
+        questionID: req.params.qID,
+        answerID: req.params.aID,
+        vote: req.params.dir
     });
 });
 
